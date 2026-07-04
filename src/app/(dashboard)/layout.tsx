df96e1a6dcc24ede5b27aa5078b68ac500e163ea
Zyad <zyad.kamal1706859@science.menofia.edu.eg>
2026-07-04 03:24:24 +0300

    feat: add dashboard layout with TopBar component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,7 +6,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex flex-col bg-slate-50 dark:bg-[#060818]/60">
+    <div className="dashboard-layout min-h-screen flex flex-col">
       <TopBar />
       <main className="flex-1">{children}</main>
     </div>
